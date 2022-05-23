9fb4f8728362b9aaf692a32b6cc9db823bdbd005
babak Mirhosseini <sbabakm@gmail.com>
2022-05-23 11:09:31 +0430

    90

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -7,9 +7,91 @@
 
     @slot('script')
         <script>
+
             $('#categories').select2({
                 'placeholder' : 'دسترسی مورد نظر را انتخاب کنید'
             });
+
+
+            let changeAttributeValues = (event , id) => {
+                let valueBox = $(`select[name='attributes[${id}][value]']`);
+
+                $.ajaxSetup({
+                    headers : {
+                        'X-CSRF-TOKEN' : document.head.querySelector('meta[name="csrf-token"]').content,
+                        'Content-Type' : 'application/json'
+                    }
+                })
+
+                $.ajax({
+                    type : 'POST',
+                    url : '/admin/attribute/values',
+                    data : JSON.stringify({
+                        name : event.target.value
+                    }),
+                    success : function(data) {
+                        valueBox.html(`
+                            <option selected>انتخاب کنید</option>
+                            ${
+                            data.data.map(function (item) {
+                                return `<option value="${item}">${item}</option>`
+                            })
+                        }
+                        `);
+
+                        $('.attribute-select').select2({ tags : true });
+                    }
+                });
+            }
+
+            let createNewAttr = ({ attributes , id }) => {
+                return `
+                    <div class="row" id="attribute-${id}">
+                        <div class="col-5">
+                            <div class="form-group">
+                                 <label>عنوان ویژگی</label>
+                                 <select name="attributes[${id}][name]" onchange="changeAttributeValues(event, ${id});" class="attribute-select form-control">
+                                    <option value="">انتخاب کنید</option>
+                                    ${
+                    attributes.map(function(item) {
+                        return `<option value="${item}">${item}</option>`
+                    })
+                }
+                                 </select>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <div class="form-group">
+                                 <label>مقدار ویژگی</label>
+                                 <select name="attributes[${id}][value]" class="attribute-select form-control">
+                                        <option value="">انتخاب کنید</option>
+                                 </select>
+                            </div>
+                        </div>
+                         <div class="col-2">
+                            <label >اقدامات</label>
+                            <div>
+                                <button type="button" class="btn btn-sm btn-warning" onclick="document.getElementById('attribute-${id}').remove()">حذف</button>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }
+
+            $('#add_product_attribute').click(function() {
+                let attributesSection = $('#attribute_section');
+                let id = attributesSection.children().length;
+
+                attributesSection.append(
+                    createNewAttr({
+                        attributes : [],
+                        id
+                    })
+                );
+
+                $('.attribute-select').select2({ tags : true });
+            });
+
         </script>
     @endslot
 
@@ -51,6 +133,12 @@
                                 @endforeach
                             </select>
                         </div>
+                        <h6>ویژگی محصول</h6>
+                        <hr>
+                        <div id="attribute_section">
+
+                        </div>
+                        <button class="btn btn-sm btn-danger" type="button" id="add_product_attribute">ویژگی جدید</button>
                     </div>
                     <!-- /.card-body -->
                     <div class="card-footer">
