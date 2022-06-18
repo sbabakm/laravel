177a36bb42ff067c75a846bb62d12cfe2a9bdf6e
babak Mirhosseini <sbabakm@gmail.com>
2022-06-18 14:43:58 +0430

    108 - ostad

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,17 +12,28 @@ class PaymentController extends Controller
         //dd(Cart::instance('roocket')->all());
         $cart = Cart::instance('roocket')->all();
 
-        $totalPrice = $cart->sum(function ($cart) {
-            return $cart['product']->price * $cart['quantity'];
-        });
+        if($cart->count()){
+            $totalPrice = $cart->sum(function ($cart) {
+                return $cart['product']->price * $cart['quantity'];
+            });
 
-        $order =  auth()->user()->orders()->create([
-            'price' => $totalPrice,
-            'status' => 'unpaid',
-        ]);
+            $order =  auth()->user()->orders()->create([
+                'price' => $totalPrice,
+                'status' => 'unpaid',
+            ]);
 
-        $cart->each(function ($item, $key) use ($order) {
-            $order->products()->attach($item['product']->id , ['quantity' => $item['quantity']]);
-        });
+//        $cart->each(function ($item, $key) use ($order) {
+//            $order->products()->attach($item['product']->id , ['quantity' => $item['quantity']]);
+//        });
+
+            $orderItems = $cart->mapWithKeys(function ($item, $key) {
+                return [$item['product']->id => [ 'quantity' => $item['quantity']]];
+            });
+
+            $order->products()->attach($orderItems);
+
+            return 'ok';
+        }
+
     }
 }
